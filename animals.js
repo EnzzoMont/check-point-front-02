dff70b7f244b37679578ef0d2f60afe88b92753f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:34:39 -0300

    commitando

---
+++ animals.js
@@ -0,0 +1,8 @@
+const animais = ["Guepardo", "corvo", "elefante", "gato", "papagaio", "dromedário"]
+
+if (animais.includes("leão")){
+    console.log("O leão não está na lista")
+}
+else{
+    console.log("O leão está na lista")
+}
---
+++ series.js
@@ -0,0 +1,8 @@
+ const my3FavoriteTVShows = ["Bastardos_Inglorios", "Pica_Pau", "Chaves"]
+
+ if (my3FavoriteTVShows.length === 3){
+    console.log(True)
+ }
+
+
+

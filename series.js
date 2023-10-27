4850e89da9d550f7c7ba2875e7bfc20cae18744a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:53:16 -0300

    arrumei

--- series.js
+++ series.js
@@ -1,8 +1,11 @@
- const my3FavoriteTVShows = ["Bastardos_Inglorios", "Pica_Pau", "Chaves"]
+const my3FavoriteTVShows = ["Bastardos_Inglorios", "Pica_Pau", "Chaves"]
 
- if (my3FavoriteTVShows.length === 3){
-    console.log(True)
- }
+if (my3FavoriteTVShows.length === 3){
+  console.log(true)
+}
+else {
+  console.log(false)
+}
 
 
 
